a0e0367c588d059c9ae14bbf484b683fde370bb8
EmilianoCiurcilo <ciurcilo03@gmail.com>
2023-11-11 09:04:11 -0300

    primer entrega

--- e-commerce/src/components/NavBar/NavBar.jsx
+++ e-commerce/src/components/NavBar/NavBar.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import './NavBar.css'
 import CartWidjet from '../CartWidjet/CartWidjet'
-
+import logo from '../../assets/img/logo.png'
 
 const NavBar = () => {
   return (
     <div className='navbar-conteiner'>
+      <img src={logo} className='logo'/>
       <nav className='navbar'>
-        
         <ul className='lista'>
           <li>MOUSE</li>
           <li>TECLADO</li>
