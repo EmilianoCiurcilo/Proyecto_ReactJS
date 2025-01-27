f40cc01f0aa2e1e2a8cba5a42ee895c3ade3fde6
Emiliano Ciurcilo <Emilianociurcilo@gmail.com>
2025-01-26 20:54:28 -0600

    Actualizacion V2

--- e-commerce/src/data/productos.jsx
+++ e-commerce/src/data/productos.jsx
@@ -6,7 +6,7 @@ export const productos = [
     precio: 80000,
     descripcion: "Desde 2005 Razer es la marca líder de estilo de vida para jugadores. Ha diseñado y construido el ecosistema de hardware, software y servicios más grande del mundo. La ubicación de cada botón y curva de los mouses Razer se crearon para adaptarse perfectamente a tu mano. Gracias a estos dispositivos, conseguirás una experiencia de juego cómoda y placentera.    ",
     cantidad: 5,
-    img:"../assets/img/razer-deathadder-v2.jfif"
+    img:"/img/razer-deathadder-v2.jfif"
     },
     {
     id: 1,
@@ -15,7 +15,7 @@ export const productos = [
     precio: 50000,
     descripcion:"Logitech diseña productos y experiencias que ocupan un lugar cotidiano en la vida de las personas, poniendo foco en la innovación y la calidad. Su objetivo es crear momentos verdaderamente únicos y significativos para sus usuarios. Los mouses Logitech se adaptan a la forma de tu mano para proporcionarte horas de comodidad. Sin necesidad de mover el brazo para deslizar el cursor, tu mano se fatigará menos. Son ideales para cualquier espacio de trabajo y quienes tienen la mesa llena de diversos objetos.",
     cantidad: 5,
-    img:"../assets/img/logitech-g305.jfif"
+    img:"/img/logitech-g305.jfif"
     },
     {
     id: 2,
@@ -24,7 +24,7 @@ export const productos = [
     precio: 30000,
     descripcion:"Con más de 20 años de experiencia en fabricación de productos, Redragon innova día a día en diseño y calidad. Su objetivo es producir equipamiento de alta gama para jugadores, con excelentes prestaciones y a un precio accesible. Los mouses Redragon son adecuados para todas las ocasiones, ya sea para entretenerse en el hogar o usarlo en el trabajo. Experimentá el diseño cómodo y elegante de este dispositivo.",
     cantidad: 5,
-    img:"../assets/img/redragon-invader.jpg"
+    img:"/img/redragon-invader.jpg"
     },
     {
     id: 3,
@@ -33,7 +33,7 @@ export const productos = [
     precio: 90000,
     descripcion:"El Teclado Redragon Fizz Pro K616-rgb Rosa/blanco es el complemento perfecto para los gamers que buscan estilo y funcionalidad en un solo dispositivo. Con su retroiluminación RGB, podrás personalizar el color de las teclas según tu preferencia, creando una experiencia visual única mientras juegas. Además, su diseño compacto lo hace ideal para espacios reducidos, sin comprometer la comodidad al escribir.",
     cantidad: 5,
-    img:"../assets/img/redragon-fizzpro.jpg" 
+    img:"/img/redragon-fizzpro.jpg" 
     },
     {  
     id: 4,
@@ -42,7 +42,7 @@ export const productos = [
     precio: 130000,
     descripcion:"Hablar de Razer dentro del mundo gamer es palabra mayor. Esta compañía ofrece productos de muy alta calidad a sus usuarios. Su línea de teclados es reconocida por haber recibido distintos galardones y premios internacionales por su alto desempeño.",
     cantidad: 5,
-    img:"../assets/img/razer-blackwidow.webp" 
+    img:"/img/razer-blackwidow.webp" 
     },
     {
     id: 5,
@@ -51,7 +51,7 @@ export const productos = [
     precio: 100000,
     descripcion:"Sumérgete en la experiencia de juego definitiva con el teclado gamer HyperX Alloy Origins 60. Este teclado QWERTY en inglés US cuenta con switches HyperX Lineal Red que ofrecen una respuesta rápida y precisa, mejorando tu rendimiento en cada partida. Además, su diseño ergonómico te brinda comodidad durante largas sesiones de juego, permitiéndote disfrutar al máximo sin fatiga.",
     cantidad: 5,
-    img:"../assets/img/hyperex-alloy.jpg"
+    img:"/img/hyperex-alloy.jpg"
     },
     {
     id: 6,
@@ -60,7 +60,7 @@ export const productos = [
     precio: 120000,
     descripcion:"Ideado para los gamers más exigentes, el HyperX Cloud Flight tiene todo lo que un jugador busca. Su conectividad inalámbrica permite moverte de un lugar a otro sin cables. A su vez, su batería de larga duración te dará horas interminables de sonidos envolventes en tus partidas. Sus orejeras son sinónimo de comodidad, ya que pueden rotar en un ángulo de 90 grados y reposar de forma suave en tu cuello. Y como si esto fuera poco, en estas vas a controlar los efectos de luz LED, silenciar o encender el micrófono con cancelación de ruido y regular el volumen.",
     cantidad: 5,
-    img:"../assets/img/hyperex-cloudFlightS.webp"
+    img:"/img/hyperex-cloudFlightS.webp"
     },
     {
     id: 7,
@@ -69,7 +69,7 @@ export const productos = [
     precio: 90000,
     descripcion:"¡Experimentá la adrenalina de sumergirte en la escena de otra manera! Tener auriculares específicos para jugar cambia completamente tu experiencia en cada partida. Con los Razer Kraken Kitty BT Edition no te perdés ningún detalle y escuchás el audio tal y como fue diseñado por los creadores.",
     cantidad: 5,
-    img:"../assets/img/razer-kraken.png"
+    img:"/img/razer-kraken.png"
     },
     {
     id: 8,
@@ -78,7 +78,7 @@ export const productos = [
     precio: 70000,
     descripcion:"¡Experimentá la adrenalina de sumergirte en la escena de otra manera! Tener auriculares específicos para jugar cambia completamente tu experiencia en cada partida. Con los PlayStation Pulse 3D no te perdés ningún detalle y escuchás el audio tal y como fue diseñado por los creadores.",
     cantidad: 5,
-    img:"../assets/img/ps5-pulse.jfif"
+    img:"/img/ps5-pulse.jfif"
     },
     {
     id: 9,
@@ -87,7 +87,7 @@ export const productos = [
     precio: 150000,
     descripcion:"Con el micrófono HyperX descubrirás una nueva forma de captar y emitir audios. Fiel reflejo de la realidad Ideal para varias actividades. Te brindará un sonido de calidad y conseguirás la nitidez de las voces. Un formato a tu medida Al ser condensador, posibilitará un resultado claro y fino. Es ideal para percusiones, guitarras, pianos, entre otros. Por su respuesta tan definida ante la voz, es el más elegido por los profesionales.",
     cantidad: 5,
-    img:"../assets/img/hyperex-quadcast-s.jfif"
+    img:"/img/hyperex-quadcast-s.jfif"
     },
     {
     id: 10,
@@ -96,7 +96,7 @@ export const productos = [
     precio: 500000,
     descripcion:"Encontrá el micrófono más adecuado para tus necesidades, tanto si buscás uno vocal, de instrumento o inalámbrico. Desde 1925, artistas y entusiastas de la música de todo el mundo han elegido a Shure por el rendimiento incomparable de sus productos. Fiel reflejo de la realidad. Con este producto lograrás que la reproducción obtenida sea lo más parecida a la original. Excelente para grabar voces debido a su sensibilidad y amplio rango de frecuencia.",
     cantidad: 5,
-    img:"../assets/img/shure-sm7b.jfif"
+    img:"/img/shure-sm7b.jfif"
     },
     {
     id: 11,
@@ -105,7 +105,7 @@ export const productos = [
     precio: 40000,
     descripcion:"Con el micrófono HyperX descubrirás una nueva forma de captar y emitir audios. Fiel reflejo de la realidad Ideal para varias actividades. Te brindará un sonido de calidad y conseguirás la nitidez de las voces. Un formato a tu medida Al ser condensador, posibilitará un resultado claro y fino. Es ideal para percusiones, guitarras, pianos, entre otros. Por su respuesta tan definida ante la voz, es el más elegido por los profesionales.",
     cantidad: 5,
-    img:"../assets/img/hyperex-solocast.jfif"
+    img:"/img/hyperex-solocast.jfif"
     },
     {
     id: 12,
@@ -114,7 +114,7 @@ export const productos = [
     precio: 125000,
     descripcion:"Logitech es una empresa suiza con alcance internacional que diseña productos y experiencias para acompañar las actividades cotidianas de las personas que la eligen. Cuando crear tecnología, se enfoca en la manera como sus clientes se conectan e interactúan con el mundo digital. Es por ello que esta cámara puede ser de gran utilidad en tu día a día.",
     cantidad: 5,
-    img:"../assets/img/logitech-brio.webp"
+    img:"/img/logitech-brio.webp"
     },
     {
     id: 13,
@@ -123,7 +123,7 @@ export const productos = [
     precio: 70000,
     descripcion:"Logitech es una empresa suiza con alcance internacional que diseña productos y experiencias para acompañar las actividades cotidianas de las personas que la eligen. Cuando crear tecnología, se enfoca en la manera como sus clientes se conectan e interactúan con el mundo digital. Es por ello que esta cámara puede ser de gran utilidad en tu día a día.",
     cantidad: 5,
-    img:"../assets/img/logitech-c920.jfif"
+    img:"/img/logitech-c920.jfif"
     },
     {
     id: 14,
@@ -132,7 +132,7 @@ export const productos = [
     precio: 800000,
     descripcion:"Sony, marca líder a nivel mundial en el ámbito de la fotografía, fabrica productos de alta performance, sin descuidar la elegancia en sus diseños. Fotos en todo momento. Esta cámara se adapta a tu ritmo de vida y te ofrece excelentes resultados en situaciones complejas. En situaciones con poca iluminación o cuando necesitás realizar tomas de escenas rápidas, la cámara sin espejos sortea todos los obstáculos para ofrecer fotografías nítidas y videos en alta calidad. Su peso ligero y resultados increíbles la convierten en tu compañera ideal.",
     cantidad: 5,
-    img:"../assets/img/camara-sony.jpg"
+    img:"/img/camara-sony.jpg"
     },
     {
     id: 15,
@@ -141,7 +141,7 @@ export const productos = [
     precio: 110000,
     descripcion:"Control preciso, Este mando combina funciones revolucionarias mientras conserva precisión, comodidad y exactitud en cada movimiento. Gracias a su ergonomía especialmente pensada para la posición de tu mano, podés pasar horas jugando con total confort. Mayor comodidad y realismo. Te permite jugar sin necesidad de que haya cables de por medio. Está pensado no solamente para controlar mejor tus videojuegos, sino también para aumentar su realismo y experiencia.",
     cantidad: 5,
-    img:"../assets/img/xbox-elite.jfif"
+    img:"/img/xbox-elite.jfif"
     },
     {
     id: 16,
@@ -150,7 +150,7 @@ export const productos = [
     precio: 75000,
     descripcion:"Control preciso, Este mando combina funciones revolucionarias mientras conserva precisión, comodidad y exactitud en cada movimiento. Gracias a su ergonomía especialmente pensada para la posición de tu mano, podés pasar horas jugando con total confort. Mayor comodidad y realismo. Te permite jugar sin necesidad de que haya cables de por medio. Está pensado no solamente para controlar mejor tus videojuegos, sino también para aumentar su realismo y experiencia.",
     cantidad: 5,
-    img:"../assets/img/joystick-ps5.webp"
+    img:"img/joystick-ps5.webp"
     },
     {
     id: 17,
@@ -159,7 +159,7 @@ export const productos = [
     precio: 25000,
     descripcion:"Control preciso, Este mando combina funciones revolucionarias mientras conserva precisión, comodidad y exactitud en cada movimiento. Gracias a su ergonomía especialmente pensada para la posición de tu mano, podés pasar horas jugando con total confort. Mayor comodidad y realismo. Te permite jugar sin necesidad de que haya cables de por medio. Está pensado no solamente para controlar mejor tus videojuegos, sino también para aumentar su realismo y experiencia.",
     cantidad: 5,
-    img:"../assets/img/joystick-redragon.jpeg"
+    img:"img/joystick-redragon.jpeg"
     }
 ]
 
