9a1c59b6dca8350a84509d3dccfeb714197083fc
Emiliano Ciurcilo <Emilianociurcilo@gmail.com>
2025-01-26 21:14:40 -0600

    changes

--- e-commerce/src/components/Item/Item.jsx
+++ e-commerce/src/components/Item/Item.jsx
@@ -1,20 +1,20 @@
 import React from 'react'
 import { Card, Image, Stack, Heading, Text, Divider, ButtonGroup, Button, CardBody, CardFooter } from '@chakra-ui/react'
 import ItemCount from '../ItemCount/ItemCount'
-import  { Productos } from '../../data/productos'
+import img from '../../assets/img/'
 import { Link } from 'react-router-dom'
 const Item = ({ id, nombre, img, precio }) => {
 
   return (
-    <Card maxW='sm' >
+    <Card maxW='sm'>
       <CardBody>
         <Image
-          src={Productos.img}
+          src={`/assets/${img}`}
           alt={nombre}
           borderRadius='lg'
           boxSize='100%'
-          objectFit='cover' 
-          h='400px' 
+          objectFit='cover'
+          h='400px'
         />
         <Stack mt='6' spacing='3'>
           <Heading size='md'>{nombre}</Heading>
@@ -26,9 +26,9 @@ const Item = ({ id, nombre, img, precio }) => {
       <Divider />
       <CardFooter>
         <ButtonGroup spacing='2'>
-          <Button variant='solid' colorScheme='blue' padding={'25px'} marginLeft={'100px'} >
+          <Button variant='solid' colorScheme='blue' padding={'25px'} marginLeft={'100px'}>
             <Link to={`/product/${id}`}>
-                Ver Producto
+              Ver Producto
             </Link>
           </Button>
         </ButtonGroup>
